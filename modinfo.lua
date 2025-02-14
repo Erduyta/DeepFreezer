725b673f151bf92ab8f16c2d01fd135035e21f8f
Erduyta <88354309+Erduyta@users.noreply.github.com>
2025-02-14 17:34:39 +0400

    feat-recipe-difficulty

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 name = "Deep freezer"
 description = "Adds deep dark fantasy freezer to the game!\nA new end game refrigerator that has 12 slots and stops food spoilage."
 author = "Erdutya & Buu"
-version = "2.1"
+version = "1.0.1"
 
 api_version = 10
 
@@ -15,4 +15,17 @@ icon = "deep_freezer.tex"
 
 configuration_options =
 {
+    {
+		name = "RECIPE",
+		label = "Recipe difficulty",
+		hover = "Adjust the recipe.",
+		options =	{
+						{description = "Normal", data = 0, hover = "2 cutstones; 4 bundle wraps; 1 blue gem;"},
+                        {description = "Hard (2 gears)", data = 1, hover = "2 gears; 6 bundle wraps; 2 blue gems;"},
+                        {description = "Hard (4 gears)", data = 2, hover = "4 gears; 6 bundle wraps; 2 blue gems;"},
+						{description = "Hard (8 gears)", data = 3, hover = "8 gears; 6 bundle wraps; 2 blue gems;"},
+                        {description = "Post-lunar", data = 4, hover = "1 restrained static; 2 gears; 4 scraps;"}
+					},
+		default = 0,
+	},
 }
--- modmain.lua
+++ modmain.lua
@@ -20,25 +20,90 @@ GLOBAL.STRINGS.NAMES.DEEP_FREEZER = "Deep Freezer"
 STRINGS.RECIPE_DESC.DEEP_FREEZER = 'Make your food go "brrrr..."'
 GLOBAL.STRINGS.CHARACTERS.GENERIC.DESCRIBE.DEEP_FREEZER = "Fresh food, but cold!"
 
-local Spot = GetModConfigData("Spot")
-local eightxten = (GetModConfigData("eightxten") == "8x10")
-GLOBAL.yep = (GetModConfigData("workit") == "yep")
-GLOBAL.chillit = (GetModConfigData("chillit") == "yep")
+local recipe_difficulty = GetModConfigData("RECIPE")
 
-AddRecipe2("deep_freezer",
-    {
-        Ingredient("cutstone", 2),
-        Ingredient("bundlewrap", 4),
-        Ingredient("bluegem", 1),
-    },
-    TECH.NONE,  -- Нет специальной технологии
-    {
-        atlas = "images/inventoryimages/deep_freezer.xml",  -- Атлас для изображения предмета
-        image = "deep_freezer.tex",  -- Изображение предмета
-        placer = "common/deep_freezer_placer"
-    },
-    { "CONTAINERS" }  -- Вкладка, на которой будет отображаться рецепт
-)
+if recipe_difficulty == 0 then
+    AddRecipe2("deep_freezer",
+        {
+            Ingredient("cutstone", 2),
+            Ingredient("bundlewrap", 4),
+            Ingredient("bluegem", 1),
+        },
+        TECH.NONE,
+        {
+            atlas = "images/inventoryimages/deep_freezer.xml",
+            image = "deep_freezer.tex",
+            placer = "common/deep_freezer_placer"
+        },
+        { "CONTAINERS" }
+    )
+end
+if recipe_difficulty == 1 then
+    AddRecipe2("deep_freezer",
+        {
+            Ingredient("cutstone", 2),
+            Ingredient("bundlewrap", 6),
+            Ingredient("gears", 2),
+        },
+        TECH.NONE,
+        {
+            atlas = "images/inventoryimages/deep_freezer.xml",
+            image = "deep_freezer.tex",
+            placer = "common/deep_freezer_placer"
+        },
+        { "CONTAINERS" }
+    )
+end
+if recipe_difficulty == 2 then
+    AddRecipe2("deep_freezer",
+        {
+            Ingredient("cutstone", 2),
+            Ingredient("bundlewrap", 6),
+            Ingredient("gears", 4),
+        },
+        TECH.NONE,
+        {
+            atlas = "images/inventoryimages/deep_freezer.xml",
+            image = "deep_freezer.tex",
+            placer = "common/deep_freezer_placer"
+        },
+        { "CONTAINERS" }
+    )
+end
+if recipe_difficulty == 3 then
+    AddRecipe2("deep_freezer",
+        {
+            Ingredient("cutstone", 2),
+            Ingredient("bundlewrap", 6),
+            Ingredient("gears", 8),
+        },
+        TECH.NONE,
+        {
+            atlas = "images/inventoryimages/deep_freezer.xml",
+            image = "deep_freezer.tex",
+            placer = "common/deep_freezer_placer"
+        },
+        { "CONTAINERS" }
+    )
+end
+if recipe_difficulty == 4 then
+    AddRecipe2("deep_freezer",
+        {
+            Ingredient("wagpunk_bits", 4),
+            Ingredient("moonstorm_static_item", 1),
+            Ingredient("gears", 2),
+        },
+        TECH.NONE,
+        {
+            atlas = "images/inventoryimages/deep_freezer.xml",
+            image = "deep_freezer.tex",
+            placer = "common/deep_freezer_placer"
+        },
+        { "CONTAINERS" }
+    )
+end
+
+
 local containers = require "containers"
 containers.params["deep_freezer"] = containers.params.shadowchester
 
